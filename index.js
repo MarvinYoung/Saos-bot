b25e1e8de134a326b2658df75b0aa4a17c68df4f
MarvinYoung <69157007+MarvinYoung@users.noreply.github.com>
2020-11-13 18:24:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client({partials : ["MESSAGE","CHANNEL","REACTION"]});
 const fs = require("fs");
 let badword = JSON.parse(fs.readFileSync("badword.json", "utf8"));
-const prefix = require(process.env.PREFIX);
+const prefix = process.env.PREFIX
 
 client.on('ready',()=>{
     console.log(`Logged in as ${client.user.tag}`);
