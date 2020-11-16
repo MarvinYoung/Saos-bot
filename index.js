447916d2d05d242cd60d4a977f561db71df53ccf
MarvinYoung <69157007+MarvinYoung@users.noreply.github.com>
2020-11-16 20:50:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ let badword = JSON.parse(fs.readFileSync("badword.json", "utf8"));
 const prefix = process.env.PREFIX
 
 const updatemember = () =>{
-    const channelId = '777571163660222484';
+   const channelId = '777571163660222484';
     const channelBot = '777891515271479306';
     const guild = client.guilds.cache.get('724165603740483635');
     const channel = guild.channels.cache.get(channelId);
+    const channelB = guild.channels.cache.get(channelBot);
     channel.setName(`Members: ${guild.memberCount.toLocaleString()-guild.members.cache.filter(member => member.user.bot).size}`)
-    channelBot.setName(`Bots: ${guild.members.cache.filter(member => member.user.bot).size}`)
+    channelB.setName(`Bots: ${guild.members.cache.filter(member => member.user.bot).size}`)
   }
 client.on('guildMemberAdd',(member)=>{
     updatemember()
